bd685a93587d91ebe6710ca1f8c3802381e83588
parsasipaysa <62909491+parsasipaysa@users.noreply.github.com>
2020-05-10 15:59:16 -0400

    Update sipaylist.py

--- sipaylist.py
+++ sipaylist.py
@@ -2,14 +2,15 @@ from itertools import permutations
 from colorama import Fore
 import sys
 import os
-import pyfiglet
 from time import sleep
+import pyfiglet
 
 def xs():
     print(Fore.RED+"\n[!]"+Fore.BLUE+" if you dont know, empty enter\n")
 
 def logo():
-    print(pyfiglet.figlet_format(" S!PLIST",font = 'epic'))
+    print(Fore.RED+pyfiglet.figlet_format("sipay",font = 'epic'))
+    print(Fore.YELLOW+pyfiglet.figlet_format("list",font = 'epic'))
 def print2(arg):
     with open("sipaylist.txt", "a") as f:
         f.write(arg+"\n")    
@@ -18,25 +19,25 @@ def answer():
         try:
             os.system('clear')
             logo()
-            sleep(0.3)
+            sleep(0.2)
             print(Fore.RED+"\n[HAHA!]"+Fore.YELLOW+" MADE BY PARSA HAMIDI"+"\n")
-            sleep(0.3)
+            sleep(0.2)
             print(Fore.RED+"\n[?]"+Fore.LIGHTBLUE_EX+" How do you information?"+"\n")
-            sleep(0.3)
+            sleep(0.2)
             print(Fore.YELLOW+" \n[1]"+Fore.RED+" I'll answer yor questions\n"+Fore.YELLOW+"\n*************************")
-            sleep(0.3)
+            sleep(0.2)
             print(Fore.YELLOW+" \n[2]"+Fore.RED+" I enter the information myself\n"+Fore.YELLOW+"\n*************************")
-            sleep(0.3)
+            sleep(0.2)
             print(Fore.YELLOW+" \n[3]"+Fore.RED+" Owner Specifications\n"+Fore.YELLOW+"\n*************************")
-            sleep(0.3)
+            sleep(0.2)
             print(Fore.YELLOW+" \n[4]"+Fore.RED+" Exit\n"+Fore.YELLOW+"\n*************************")
-            sleep(0.3)
+            sleep(0.2)
             
             option = input(Fore.RED+"  ┌─["+Fore.LIGHTGREEN_EX+"Select an Option :)"+Fore.BLUE+"~"+Fore.WHITE+"TELEGRAM ID: @SIPAY"+Fore.RED+"/"+Fore.CYAN+"HACK"+Fore.RED+"/"+Fore.LIGHTYELLOW_EX+"OK..."+Fore.RED+"""]
   └──╼ """+Fore.WHITE+">> ")
             if option == "1":
                 os.system('clear')
-                lis = ["_", "#", "@"]
+                lis = []
                 logo()
                 xs()
                 a = input(Fore.RED+"  ┌─["+Fore.LIGHTGREEN_EX+"What is a person's first name? "+Fore.BLUE+"~"+Fore.WHITE+"TELEGRAM ID: @SIPAY"+Fore.RED+"/"+Fore.CYAN+"HACK"+Fore.RED+"/"+Fore.LIGHTYELLOW_EX+"OK..."+Fore.RED+"""]
@@ -133,6 +134,107 @@ def answer():
                     for permutation in permutations(lis,i):
                         x = (''.join([str(p) for p in permutation]))
                         print2(x)
+                has = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"# "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if has == "y" or has == "Y":
+                    lis.append("#")
+                else:
+                    pass
+
+                star = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"* "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if star == "y" or star == "Y":
+                    lis.append("*")
+                else:
+                    pass
+
+                plus = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"+ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if plus == "y" or plus == "Y":
+                    lis.append("+")
+                else:
+                    pass
+
+                dar = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"% "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if dar == "y" or dar == "Y":
+                    lis.append("%")
+                else:
+                    pass
+
+                boz = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"> "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if boz == "y" or boz == "Y":
+                    lis.append(">")
+                else:
+                    pass
+
+                koc = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"< "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if koc == "y" or koc == "Y":
+                    lis.append("<")
+                else:
+                    pass
+
+                under = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"_ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if under == "y" or under == "Y":
+                    lis.append("_")
+                else:
+                    pass
+
+                do = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+": "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if do == "y" or do == "Y":
+                    lis.append(":")
+                else:
+                    pass
+
+                par1 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"( "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if par1 == "y" or par1 == "Y":
+                    lis.append("(")
+                else:
+                    pass
+
+                par2 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+") "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if par2 == "y" or par2 == "Y":
+                    lis.append(")")
+                else:
+                    pass
+
+                ad = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"@ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if ad == "y" or ad == "Y":
+                    lis.append("@")
+                else:
+                    pass
+
+                dol = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"$ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if dol == "y" or dol == "Y":
+                    lis.append("$")
+                else:
+                    pass
+
+                kur1 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"[ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if kur1 == "y" or kur1 == "Y":
+                    lis.append("[")
+                else:
+                    pass
+
+                kur2 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"] "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if kur2 == "y" or kur2 == "Y":
+                    lis.append("]")
+                else:
+                    pass
+
+                minus = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"- "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if minus == "y" or minus == "Y":
+                    lis.append("-")
+                else:
+                    pass
+
+                sl = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+r"\ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if sl == "y" or sl == "Y":
+                    lis.append("\\")
+                else:
+                    pass
+
+                sl2 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"/ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if sl2 == "y" or sl2 == "Y":
+                    lis.append("/")
+                else:
+                    pass
                 input(Fore.BLUE+'[*] '+Fore.GREEN+'OK... The Passwords Were Stored In a File Called ""sipaylist.txt""')
                 pr = input(Fore.LIGHTRED_EX+"[?]"+Fore.LIGHTBLUE_EX+" you see sipaylist.txt? y/n  ")
                 if pr == "y":
@@ -141,19 +243,120 @@ def answer():
                     print(c)
                     input(Fore.RESET+"[*]"+Fore.YELLOW+" ok!"+Fore.GREEN+" :)")
                     os.system('clear')
+                
+
             elif option == "2":
                 os.system('clear')
                 logo()
-                print(Fore.RED+"\n[!]"+Fore.YELLOW+" MADE BY PARSA HAMIDI"+"\n")
-                item = input(Fore.RED+"  ┌─["+Fore.LIGHTGREEN_EX+"PASSWORDLIST"+Fore.BLUE+"~"+Fore.WHITE+"TELEGRAM ID: @SIPAY"+Fore.RED+"/"+Fore.CYAN+"HACK"+Fore.RED+"/"+Fore.LIGHTYELLOW_EX+"OK..."+Fore.RED+"""]
+                print(Fore.RED+"\n[!]"+Fore.YELLOW+" press empty enter for go to menu"+"\n")
+                item = input(Fore.RED+"  ┌─["+Fore.LIGHTGREEN_EX+"Enter the information and separate with "+Fore.YELLOW+" , "+Fore.BLUE+"~"+Fore.WHITE+"TELEGRAM ID: @SIPAY"+Fore.RED+"/"+Fore.CYAN+"HACK"+Fore.RED+"/"+Fore.LIGHTYELLOW_EX+"OK..."+Fore.RED+"""]
   └──╼ """+Fore.WHITE+">> ")
                 if item == "":
-                    print(Fore.RED+"[!] Pleas Enter"+Fore.mro+":)")
-                    sys.exit()
+                    answer()
                 items = item.split(',')
-                items.append("_")
-                items.append("#")
-                items.append("@")
+                has = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"# "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if has == "y" or has == "Y":
+                    lis.append("#")
+                else:
+                    pass
+
+                star = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"* "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if star == "y" or star == "Y":
+                    lis.append("*")
+                else:
+                    pass
+
+                plus = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"+ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if plus == "y" or plus == "Y":
+                    lis.append("+")
+                else:
+                    pass
+
+                dar = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"% "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if dar == "y" or dar == "Y":
+                    lis.append("%")
+                else:
+                    pass
+
+                boz = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"> "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if boz == "y" or boz == "Y":
+                    lis.append(">")
+                else:
+                    pass
+
+                koc = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"< "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if koc == "y" or koc == "Y":
+                    lis.append("<")
+                else:
+                    pass
+
+                under = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"_ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if under == "y" or under == "Y":
+                    lis.append("_")
+                else:
+                    pass
+
+                do = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+": "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if do == "y" or do == "Y":
+                    lis.append(":")
+                else:
+                    pass
+
+                par1 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"( "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if par1 == "y" or par1 == "Y":
+                    lis.append("(")
+                else:
+                    pass
+
+                par2 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+") "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if par2 == "y" or par2 == "Y":
+                    lis.append(")")
+                else:
+                    pass
+
+                ad = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"@ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if ad == "y" or ad == "Y":
+                    lis.append("@")
+                else:
+                    pass
+
+                dol = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"$ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if dol == "y" or dol == "Y":
+                    lis.append("$")
+                else:
+                    pass
+
+                kur1 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"[ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if kur1 == "y" or kur1 == "Y":
+                    lis.append("[")
+                else:
+                    pass
+
+                kur2 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"] "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if kur2 == "y" or kur2 == "Y":
+                    lis.append("]")
+                else:
+                    pass
+
+                minus = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"- "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if minus == "y" or minus == "Y":
+                    lis.append("-")
+                else:
+                    pass
+
+                sl = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+r"\ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if sl == "y" or sl == "Y":
+                    lis.append("\\")
+                else:
+                    pass
+
+                sl2 = input(Fore.LIGHTBLUE_EX+"\nDo you want to be added to the passlist "+Fore.YELLOW+"/ "+Fore.RED+"? "+Fore.GREEN+"y"+Fore.WHITE+"/"+Fore.RED+"n  ")
+                if sl2 == "y" or sl2 == "Y":
+                    lis.append("/")
+                else:
+                    pass
+
+                
                 print(Fore.BLUE+"[+]"+Fore.YELLOW+" Pleas wait")
                 for i in range(1,len(items)+1):
                     for permutation in permutations(items,i):
@@ -170,7 +373,7 @@ def answer():
                 else:
                     os.system('clear')
             elif option == "4":
-                print("\n"+Fore.RED+"[!] your exit"+Fore.BLUE+" :)")
+                print("\n"+Fore.RED+"[!] your exit"+Fore.BLUE+" :)\n")
                 sys.exit()
             elif option == "3":
                 os.system('clear')
@@ -180,12 +383,11 @@ def answer():
 
                 Email : parsa.hamidi70@gmail.com
                 
-                Telegram ID : @sipay
+                Telegram ID : https://T.me/sipay
                 
                 """)
                 input(Fore.BLUE+'\n[*] '+Fore.YELLOW+'OK... (Press Enter to return to the menu!...)')
         except:
-            print("\n"+Fore.RED+"[!] your exit"+Fore.BLUE+" :)")
             sys.exit()
 
 answer()
